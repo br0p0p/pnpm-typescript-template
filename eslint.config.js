3f83947edd0f5dc1107c2c36ce48142391036aba
Caleb Williams <657922+br0p0p@users.noreply.github.com>
2025-12-02 06:16:20 +0000

    Add prettier back, clean up CI stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  formatters: true,
+  formatters: {
+    css: true,
+    html: true,
+    markdown: 'prettier',
+    yaml: true,
+  },
   pnpm: true,
   typescript: {
     tsconfigPath: 'tsconfig.json',
